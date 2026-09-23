9e6b8ab89ea8ea077bdbbec380a4d65c92e2d215
fullstacklover <adamdieng289@gmail.com>
2026-09-23 19:33:26 +0000

    Configuration du serveur

--- index.js
+++ index.js
@@ -2,4 +2,14 @@ import express from "express";
 
 const app = express();
 
+
+app.get("/", (req, res) => {
+    return res.status(200).json(
+         {
+            success : true,
+            message : "Hello from backend!"
+         }
+    )
+});
+
 app.listen(4000)
